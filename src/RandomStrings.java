2a8b97575cfa3bebb7b2362e00597d0a324dc490
Chase Poland <chase.poland@gmail.com>
2022-02-03 00:37:45 -0500

    just help lol

---
+++ src/RandomStrings.java
@@ -0,0 +1,43 @@
+//Name: Chase Poland
+//Class: CSPC 200
+//Date: 02/2/22
+//Version: C
+//Time: 2 hours
+//Status: 
+import java.util.*;
+
+public class RandomStrings {
+    public static void main(String[] args)
+    {
+
+        //Initialization
+        Random r = new Random();
+        long seed = 100;
+        r.setSeed(seed);
+        Scanner scan = new Scanner(System.in);
+
+
+        // C VERSION CODE
+        //PART OF C VERSION
+        String stringOne = scan.nextLine();
+        int random = r.nextInt(1,stringOne.length());
+        String sPartOne = (stringOne.substring(0, random - 1));
+        String sPartTwo = (stringOne.substring(random, stringOne.length()));
+        System.out.print(sPartOne + sPartTwo);
+
+        //B VERSION CODE
+        //PART OF B VERSION
+        //random = r.nextInt(1,stringOne.length());
+        //System.out.println(random);
+        //String movedString = (stringOne.substring(random -1, random));
+        //String movedString2 = (stringOne.substring(random -2, random -1));
+        //String sPartThree = (stringOne.substring(0,random -2));
+        //String sPartFour = (stringOne.substring(random, stringOne.length()));
+        //System.out.println(sPartThree + movedString + movedString2 + sPartFour);
+
+        
+
+
+
+    }
+}

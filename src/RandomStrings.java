12f0bdc7024e70ffd2f73ec256df326b46e2c8c3
Chase Poland <chase.poland@gmail.com>
2022-02-03 12:52:53 -0500

    lol

--- src/RandomStrings.java
+++ src/RandomStrings.java
@@ -4,6 +4,7 @@
 //Version: C
 //Time: 2 hours
 //Status: 
+
 import java.util.*;
 
 public class RandomStrings {
@@ -23,17 +24,30 @@ public class RandomStrings {
         int random = r.nextInt(1,stringOne.length());
         String sPartOne = (stringOne.substring(0, random - 1));
         String sPartTwo = (stringOne.substring(random, stringOne.length()));
-        System.out.print(sPartOne + sPartTwo);
+        System.out.println(sPartOne + sPartTwo);
 
         //B VERSION CODE
         //PART OF B VERSION
         //random = r.nextInt(1,stringOne.length());
         //System.out.println(random);
-        //String movedString = (stringOne.substring(random -1, random));
-        //String movedString2 = (stringOne.substring(random -2, random -1));
-        //String sPartThree = (stringOne.substring(0,random -2));
-        //String sPartFour = (stringOne.substring(random, stringOne.length()));
-        //System.out.println(sPartThree + movedString + movedString2 + sPartFour);
+        /*String movedString = (stringOne.substring(random -1, random));
+        String movedString2 = (stringOne.substring(random -2, random -1));
+        String sPartThree = (stringOne.substring(0,random -2));
+        String sPartFour = (stringOne.substring(random, stringOne.length()));
+        System.out.println(sPartThree + movedString + movedString2 + sPartFour);*/
+
+        //A VERSION CODE
+        //PART OF A VERSION
+        String stringA = scan.nextLine();
+        StringBuilder stringAB = new StringBuilder();
+        random = r.nextInt(1,stringA.length());
+        String stringI = (stringA.substring(0,random));
+        String stringE = (stringA.substring(random, stringA.length()));
+        stringAB.append(stringE);
+        stringAB.reverse();
+        System.out.println(random);
+        System.out.println(stringI + stringAB);
+        
 
         
 
